77cf27d06c909071da706582773f0cfa9d3e039c
lpepino <leonardodpepino@gmail.com>
2020-05-22 14:22:04 -0300

    passing params

--- autoencoders.py
+++ autoencoders.py
@@ -6,20 +6,34 @@ from save_checkpoints import SaveCheckpoints
 from IPython import embed
 import importlib
 
-def make_autoencoder(model_name, mode='load', model_weights=None):
+def make_autoencoder(model_name, mode='load', model_weights=None, train_df=None, train_params=None, model_params=None):
 
     setup_tf()
+    train_params_ = {
+        'batch_size': 3,
+        'input_size': 16384,
+        'epochs': 100,
+        'loss': SpectralLoss(),
+        'optimizer': tf.keras.optimizers.RMSprop(clipnorm=0.1,learning_rate=0.001)
+        }
+
+    if train_params:
+        train_params_.update(train_params)
+
     model_module = importlib.import_module('models.{}'.format(model_name))
     model_fn = getattr(model_module,'build_model')
-    ae_model, enc_model, dec_model = model_fn()
+    if model_params:
+        ae_model, enc_model, dec_model = model_fn(**model_params)
+    else:
+        ae_model, enc_model, dec_model = model_fn()
     
     if mode=='train':
-        optimizer = tf.keras.optimizers.RMSprop(clipnorm=0.1,learning_rate=0.001)
-        ae_model.compile(optimizer=optimizer,loss=SpectralLoss())
+        #optimizer = tf.keras.optimizers.RMSprop(clipnorm=0.1,learning_rate=0.001)
+        ae_model.compile(optimizer=train_params_['optimizer'],loss=train_params_['loss'])
         ae_model.summary()
 
-        df_drums = load_wavs_from_folders(['../data/drums'])
-        ae_model.fit(AudioGenerator(df_drums,input_size=WAV_SIZE,batch_size=3),epochs=100,callbacks=[SaveCheckpoints(monitor_metric='loss',save_best=True)])
+        #df_drums = load_wavs_from_folders(['../data/drums'])
+        ae_model.fit(AudioGenerator(train_df,input_size=train_params_['input_size'],batch_size=train_params_['batch_size']),epochs=train_params_['epochs'],callbacks=[SaveCheckpoints(monitor_metric='loss',save_best=True)])
 
     elif mode=='load':
         load_model([ae_model,enc_model,dec_model],model_weights)
